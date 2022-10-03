59e1598c4cdd3799def592071e4a0a95298f1a6b
xartola <xabiarzab@gmail.com>
2022-10-03 18:03:27 +0200

    "Proba"

--- src/test/java/edu/ncsu/monopoly/CardsTest.java
+++ src/test/java/edu/ncsu/monopoly/CardsTest.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 public class CardsTest extends TestCase {
     Card ccCard, chanceCard;   
-    
+    //dff
     GameMaster gameMaster;
 
     protected void setUp() {
